b2a0de7aaadc377ba3d6261b0c055babf74236ac
David Toro Triana <dtorot@opensai.org>
2025-02-01 18:53:02 -0500

    New Task model...

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+
+
+class Task(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Knowledge task item...",
+    )
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('task-detail', args=[str(self.id)])
+    
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='task_name_case_insensitive_unique',
+                violation_error_message="Task already exists (case insensitive match)"
+            ),
+        ]
+
+    
