aac7a539e5894b593fd8b233762f19d1d392e4df
David Toro Triana <dtorot@opensai.org>
2025-02-17 21:16:29 -0500

    New List Detail Template... (Learning Path)

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from .models import Task, Path, Learning, Guache
+
+class PathListView(generic.ListView):
+    model = Task
+    context_object_name = 'path_list'
+
+    #queryset = Path.objects.filter(name__icontains='linux')[:5]
+    #template_name = 'paths/learning_paths_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(PathListView, self).get_context_data(**kwargs)
+        context = ['some_data'] = 'This is just some data'
+
+        return context
+
+
 
 def index(request):
     num_paths = Task.objects.all().count()
