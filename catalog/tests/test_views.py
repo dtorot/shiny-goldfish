c6c2659acd5fe354e1f32105cfa381f32221d10e
nemqueteba <nemqueteba@gmail.com>
2025-09-01 21:00:00 -0500

    ...testing views in progress, redirection urls in login is the next step

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -1,3 +1,120 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.urls import reverse
+
+from catalog.models import Author
+
+import datetime
+
+from django.utils import timezone
+
+# Get user model from settings
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from catalog.models import Learning, Path, Task, Guache
+
+
+class GuacheListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create 13 authors for pagination tests
+        number_of_guaches = 13
+
+        for guache_id in range(number_of_guaches):
+            Guache.objects.create(
+                first_name=f'Dominique {guache_id}',
+                last_name=f'Surname {guache_id}',
+            )
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/catalog/guaches/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('guaches'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('guaches'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/guache_list.html')
+
+    def test_pagination_is_ten(self):
+        response = self.client.get(reverse('guaches'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] == True)
+        self.assertEqual(len(response.context['guache_list']), 10)
+
+    def test_lists_all_guaches(self):
+        # Get second page and confirm it has (exactly) remaining 3 items
+        response = self.client.get(reverse('guaches')+'?page=2')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] == True)
+        self.assertEqual(len(response.context['guache_list']), 3)
+
+
+class LoanedBookInstancesByUserListViewTest(TestCase):
+    def setUp(self):
+        # Create two users
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user2 = User.objects.create_user(username='testuser2', password='2HJ1vRV0Z&3iD')
+
+        test_user1.save()
+        test_user2.save()
+
+        # Create a book
+        test_guache = Guache.objects.create(first_name='Dominique', last_name='Rousseau')
+        test_path = Path.objects.create(name='Fantasy')
+        #test_language = Language.objects.create(name='English')
+        test_task = Task.objects.create(
+            name='Task Name',
+#            summary='My book summary',
+#            isbn='ABCDEFG',
+            author=test_guache,
+#            language=test_language,
+        )
+
+        # Create genre as a post-step
+        path_objects_for_task = Path.objects.all()
+        test_task.creator.set(path_objects_for_task) # Direct assignment of many-to-many types not allowed.
+        test_task.save()
+
+        # Create 30 BookInstance objects
+        number_of_learning_copies = 30
+        for learning_copy in range(number_of_learning_copies):
+            due_back = timezone.localtime() + datetime.timedelta(days=learning_copy%5)
+            the_apprentice = test_user1 if learning_copy % 2 else test_user2
+            status = 'd'
+            Learning.objects.create(
+                path=test_book,
+                name='Unlikely Imprint, 2016',
+                due_back=due_back,
+                apprentice=the_apprentice,
+                status=status,
+            )
+
+'''
+#to-do 
+#"To verify that the view will redirect to a login page if the user..."
+#LocalLibrary test
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('my-borrowed'))
+        self.assertRedirects(response, '/accounts/login/?next=/catalog/mybooks/')
+
+    def test_logged_in_uses_correct_template(self):
+        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.get(reverse('my-borrowed'))
+
+        # Check our user is logged in
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        # Check that we got a response "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check we used correct template
+        self.assertTemplateUsed(response, 'catalog/bookinstance_list_borrowed_user.html')
+'''
